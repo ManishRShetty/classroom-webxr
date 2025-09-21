3bfd3e359c8790b07e127dcc1a9dbad276d12ce8
dhanuvagman006 <dhanushinsit@gmail.com>
2025-09-22 05:20:08 +0530

    my update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,7 @@ import SketchfabViewer from '@/components/SketchfabViewer';
 export default function HomePage() {
   return (
     <main>
-      <h1>Welcome to My Classroom</h1>
-      <p>Here is an interactive 3D model of a virtual classroom:</p>
-
       <SketchfabViewer />
-
-      <p>More content can go here...</p>
     </main>
   );
 }
