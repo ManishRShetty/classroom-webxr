8888a79dc54ba337e3f538d5b74854079e64a0ab
dhanuvagman006 <dhanushinsit@gmail.com>
2025-09-22 05:23:33 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-import type { NextConfig } from "next";
+  import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '/classroom-webxr',
+  assetPrefix: '/classroom-webxr/'
 };
 
 export default nextConfig;
